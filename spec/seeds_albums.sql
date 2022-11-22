da6b9a2560e17652271c4c9b5211a740af7adb00
Mike Oliver <remoteunderscore@gmail.com>
2022-11-22 12:02:50 +0000

    Databases - Music Library Exercise files

---
+++ spec/seeds_albums.sql
@@ -0,0 +1,15 @@
+-- Write your SQL seed here.
+
+
+
+-- First, you'd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
+
+-- TRUNCATE TABLE albums RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+-- INSERT INTO albums (title, release_year, artist_id) VALUES('Functional Design', '2022', '6' );
+-- INSERT INTO albums (title, release_year, artist_id) VALUES('Altering Illusions', '2012', '6');
